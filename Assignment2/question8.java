0332ebad2f415dc62445f4fe26fd39248c6bebc6
navo <46191545+navo@users.noreply.github.com>
2020-07-17 01:27:06 +0530

    Create question8.java

---
+++ Assignment2/question8.java
@@ -0,0 +1,16 @@
+public class question8 {
+ 
+	public static void main(String ar[])
+	{
+		for(int i=5;i>=1;i--)
+		{
+			for(int j=i-1;j>=1;j--)
+				System.out.print(" ");
+			for(int k=i;k<=5;k++)
+				System.out.print(k);
+			for(int l=i;l<=4;l++)
+				System.out.print(l);
+			System.out.println("");
+		}
+	}
+}
